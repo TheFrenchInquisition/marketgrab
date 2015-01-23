7b9f8d2a9064765ed410a453821c0917846293b4
Zerf <zerfgog202@gmail.com>
2015-01-22 19:47:01 -0700

    Create steam_python.py

--- steam_python.py
+++ steam_python.py
@@ -1,9 +1,20 @@
-import urllib.request
+import urllib.request, json
 
-api_key = "" #http://steamcommunity.com/dev/apikey
-api_url = "http://api.steampowered.com/"
-method = "" #the thing we query, get it from https://wiki.teamfortress.com/wiki/WebAPI#General_interfaces
+def fetchMeSomeData(appid, item, currency="1", debug=False):
+	data = urllib.request.urlopen("http://steamcommunity.com/market/priceoverview/?country=US&currency="+currency+"&appid="+appid+"&market_hash_name="+item).read()
+	data = str(data)
+	data = data[2:-1]
+	data = json.loads(data)
+	if data["success"]:
+		if debug:
+			print(data)
+		else:
+			lowest = data["lowest_price"][data["lowest_price"].index(";")+1:]
+			print("Lowest price: $"+lowest)
+			print("Amount for sale: "+data["volume"])
 
-def fetchMeSomeData(itemid):
-	data = urllib.request.urlopen(api_url + "" + "?key=" + api_key).read()
-	print(data)
+currency = "1" #1 = USD, 2 = GBP, 3 = EURO, 5 = RUB
+item = "StatTrak%E2%84%A2%20P250%20%7C%20Steel%20Disruption%20%28Factory%20New%29"
+game = "730" #csgo
+
+fetchMeSomeData(game, item)
